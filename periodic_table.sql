7fb9e1c8e6a16c40fcd359dd562e4988b2b19466
Vyt Lytvynenko <106233076+vplatus@users.noreply.github.com>
2022-06-12 17:40:31 +0300

    Update periodic_table.sql
    Finished for The Periodic Table Project

--- periodic_table.sql
+++ periodic_table.sql
@@ -63,7 +63,7 @@ ALTER TABLE public.elements OWNER TO freecodecamp;
 CREATE TABLE public.properties (
     atomic_number integer NOT NULL,
     type character varying(30),
-    atomic_mass numeric(6,4) NOT NULL,
+    atomic_mass real NOT NULL,
     melting_point_celsius numeric NOT NULL,
     boiling_point_celsius numeric NOT NULL,
     type_id integer DEFAULT 0 NOT NULL
@@ -126,25 +126,25 @@ INSERT INTO public.elements VALUES (6, 'C', 'Carbon');
 INSERT INTO public.elements VALUES (7, 'N', 'Nitrogen');
 INSERT INTO public.elements VALUES (8, 'O', 'Oxygen');
 INSERT INTO public.elements VALUES (1000, 'Mt', 'moTanium');
-INSERT INTO public.elements VALUES (9, 'F', 'Flourine');
 INSERT INTO public.elements VALUES (10, 'Ne', 'Neon');
+INSERT INTO public.elements VALUES (9, 'F', 'Fluorine');
 
 
 --
 -- Data for Name: properties; Type: TABLE DATA; Schema: public; Owner: freecodecamp
 --
 
-INSERT INTO public.properties VALUES (3, 'metal', 6.9400, 180.54, 1342, 1);
+INSERT INTO public.properties VALUES (3, 'metal', 6.94, 180.54, 1342, 1);
 INSERT INTO public.properties VALUES (4, 'metal', 9.0122, 1287, 2470, 1);
-INSERT INTO public.properties VALUES (1, 'nonmetal', 1.0080, -259.1, -252.9, 2);
+INSERT INTO public.properties VALUES (1, 'nonmetal', 1.008, -259.1, -252.9, 2);
 INSERT INTO public.properties VALUES (2, 'nonmetal', 4.0026, -272.2, -269, 2);
-INSERT INTO public.properties VALUES (6, 'nonmetal', 12.0110, 3550, 4027, 2);
-INSERT INTO public.properties VALUES (7, 'nonmetal', 14.0070, -210.1, -195.8, 2);
-INSERT INTO public.properties VALUES (8, 'nonmetal', 15.9990, -218, -183, 2);
-INSERT INTO public.properties VALUES (5, 'metalloid', 10.8100, 2075, 4000, 3);
-INSERT INTO public.properties VALUES (1000, 'metalloid', 1.0000, 10, 100, 3);
-INSERT INTO public.properties VALUES (9, 'nonmetal', 18.9980, -220, -188.1, 2);
-INSERT INTO public.properties VALUES (10, 'nonmetal', 20.1800, -248.6, -246.1, 2);
+INSERT INTO public.properties VALUES (6, 'nonmetal', 12.011, 3550, 4027, 2);
+INSERT INTO public.properties VALUES (7, 'nonmetal', 14.007, -210.1, -195.8, 2);
+INSERT INTO public.properties VALUES (8, 'nonmetal', 15.999, -218, -183, 2);
+INSERT INTO public.properties VALUES (5, 'metalloid', 10.81, 2075, 4000, 3);
+INSERT INTO public.properties VALUES (9, 'nonmetal', 18.998, -220, -188.1, 2);
+INSERT INTO public.properties VALUES (10, 'nonmetal', 20.18, -248.6, -246.1, 2);
+INSERT INTO public.properties VALUES (1000, 'metalloid', 1, 10, 100, 3);
 
 
 --
